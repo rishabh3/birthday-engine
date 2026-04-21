d0c9654fbe02094098ef20855414f9803930a44f
rishabh3 <rishabh97dutt@gmail.com>
2026-04-21 22:56:26 +0530

    Setup the repo for birthday card engine

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export", // required for static hosting
+  images: {
+    unoptimized: true, // important for GitHub Pages
+  },
 };
 
 export default nextConfig;
